b1bca166453b18df0ef3eb9ce54d163e3499a32f
nhathazard <93414257+nhathazard@users.noreply.github.com>
2022-05-03 00:03:15 +0700

    loading

--- app/src/main/java/com/example/balotravel/StartActivity.java
+++ app/src/main/java/com/example/balotravel/StartActivity.java
@@ -2,23 +2,55 @@ package com.example.balotravel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class StartActivity extends AppCompatActivity {
+   private static int SPLASH_SCREEN = 5000;
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo , slogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_main);
-        BottomNavigationView bottomNavigationMenuView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
-        bottomNavigationMenuView.setBackground(null);
+        setContentView(R.layout.splashscreen);
+
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        //Hooks
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.textView);
+        slogan = findViewById(R.id.textView2);
+
+        logo.setAnimation(topAnim);
+        slogan.setAnimation(topAnim);
+        image.setAnimation(bottomAnim);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(StartActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
     }
 }
