257272bf5236c734634b928950713c15548a8f4e
ThanhTrangPhan <phanthanh.trang200@gmail.com>
2022-05-03 16:48:21 +0700

    update register and login

--- app/src/main/java/com/example/balotravel/RegisterActivity.java
+++ app/src/main/java/com/example/balotravel/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -25,6 +26,8 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_register);
         mAuth=FirebaseAuth.getInstance();
 
